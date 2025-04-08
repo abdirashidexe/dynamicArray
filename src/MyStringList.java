34ae38891428425f9a990279a50b6c89cc7a892d
Abdi <89213120+abdirashidexe@users.noreply.github.com>
2025-04-08 12:00:39 -0700

    set up 3/6 problems

--- src/MyStringList.java
+++ src/MyStringList.java
@@ -1,3 +1,26 @@
 public class MyStringList implements StringList{
-    
+    // Method #1
+    public String get(int index)
+    {
+        String specifiedIndex = "";
+
+        // return string that's at inputted index
+
+        // if inputted index is < 0 or index >= size, return error
+
+        // finally return string at inputted index
+        return specifiedIndex;
+    }
+
+    // Method #1
+    public void set(int index, String value)
+    {
+
+    }
+
+    // Method #1
+    public void add(String value)
+    {
+
+    }
 }
