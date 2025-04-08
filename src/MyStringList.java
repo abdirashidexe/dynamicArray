653c754ecf7e6dc08bb3d8250eb3de3639513af8
Kabul Totakhil <totakhilkamran7@gmail.com>
2025-04-08 12:27:54 -0700

    implemented the require code

--- src/MyStringList.java
+++ src/MyStringList.java
@@ -1,3 +1,31 @@
-public class MyStringList implements StringList{
-    
+public class MyStringList implements StringList {
+    private String[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public MyStringList() {
+        this.elements = new String[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+   
+    @Override
+    public String remove(int index) {
+        String removed = elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return removed;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public int capacity() {
+        return elements.length;
+    }
 }
